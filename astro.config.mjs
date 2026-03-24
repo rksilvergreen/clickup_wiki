dc29c3d1c26b75d863b99ba8f3ebacb1eddee54f
RK <rksilvergreen@gmail.com>
2026-03-24 15:28:57 +0200

    changes to imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        'clickup_wifi/wiki.config': fileURLToPath(new URL('./wiki.config.ts', import.meta.url)),
+        clickup_wifi: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  },
   integrations: [mdx()],
 });
