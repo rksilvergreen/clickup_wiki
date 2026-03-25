3c9034574bb7428009273562299c84b5636377f7
RK <rksilvergreen@gmail.com>
2026-03-25 12:11:33 +0200

    paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        'clickup_wifi/wiki.config': fileURLToPath(new URL('./wiki.config.ts', import.meta.url)),
-        clickup_wifi: fileURLToPath(new URL('./src', import.meta.url)),
+        'clickup_wiki/wiki.config': fileURLToPath(new URL('./wiki.config.ts', import.meta.url)),
+        clickup_wiki: fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
   },
